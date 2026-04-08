27fcb6c77908435a638df7856e0d9c37769ad5eb
ahsan waseem <ahsanwaseem@Mac.broadband>
2026-04-08 11:37:18 +0100

    new1

--- app/layout.js
+++ app/layout.js
@@ -12,14 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-black antialiased`}>
-        {/* The Navbar is now its own responsive component */}
+      <body className="bg-black">
         <Navbar />
-
-        {/* Padding-top (pt-20) ensures content starts below the navbar */}
-        <main className="pt-20"> 
-          {children}
-        </main>
+        {children}
       </body>
     </html>
   );
